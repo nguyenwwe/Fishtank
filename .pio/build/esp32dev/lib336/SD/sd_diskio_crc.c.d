e7dd5b146386ed20a1bf721ee07af0f6e6878fc8
Nguyen Pham Huy Hoang <tranvannguyen29112000@gmail.com>
2026-06-15 15:14:20 +0700

    [Upload] Update to merge 2 branch

---
+++ .pio/build/esp32dev/lib336/SD/sd_diskio_crc.c.d
@@ -0,0 +1,2 @@
+.pio/build/esp32dev/lib336/SD/sd_diskio_crc.c.o: \
+ C:/Users/Huy\ Hoang/.platformio/packages/framework-arduinoespressif32/libraries/SD/src/sd_diskio_crc.c
---
+++ .pio/build/esp32dev/src/Fishtank.cpp.d
@@ -0,0 +1,1 @@
+.pio/build/esp32dev/src/Fishtank.cpp.o: src/Fishtank.cpp
